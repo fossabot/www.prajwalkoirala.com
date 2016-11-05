0dbe21a689bd9186d872731f89aec52f74a08b9e
Prajwal Koirala <LiveChief@users.noreply.github.com>
2016-11-04 20:48:16 -0400

    Update vps_commands.php

--- Important/vps_commands.php
+++ Important/vps_commands.php
@@ -3,7 +3,7 @@
  <head>
   <meta charset="utf-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
-  <title>Important - Prajwal Koirala</title>
+  <title>vps_commands - Important - Prajwal Koirala</title>
  </head>
  <body>
 <div id="selectable">
