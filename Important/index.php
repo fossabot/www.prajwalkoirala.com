a54b9a681f0a848bfd77184da73428f49aa95a18
Prajwal Koirala <LiveChief@users.noreply.github.com>
2016-11-04 20:50:01 -0400

    Create index.php

---
+++ Important/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <title>index - Important - Prajwal Koirala</title>
+  </head>
+ <body>
+ Click <a href="https://www.prajwalkoirala.com/Important/vps_commands.php">Here</a> to go to VPS_COMMANDS.
