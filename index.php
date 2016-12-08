362d9a35ed8801961be777f1e90583fb5d3cf154
Prajwal Koirala <LiveChief@users.noreply.github.com>
2016-12-07 21:35:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -315,5 +315,5 @@
       e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){
       	(e[c].q=e[c].q||[]).push(arguments)}
       ,e[c].l=1*new Date,s=t.createElement(a),o=t.getElementsByTagName(a)[0],s.async=1,s.src=n,o.parentNode.insertBefore(s,o)}
-      (window,document,"script","https://www.google-analytics.com/analytics.js","ga"),ga("create","UA-83860703-2","auto"),ga("send","pageview")
-   </script>
+      (window,document,"script","https://www.prajwalkoirala.com/analytics.js","ga"),ga("create","UA-83860703-2","auto"),ga("send","pageview")
+      </script>
