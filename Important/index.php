4ac4e26fb1963349cf64da4bf94f97d343d796b6
LiveChief <prajwalkoirala2019@gmail.com>
2017-01-07 12:14:20 -0500

    Fixing Some Stuff

--- Important/index.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-   <meta charset="utf-8">
-   <meta name=viewport content="width=device-width, initial-scale=1">
-   <title>Index - Important - Prajwal Koirala</title>
-   <a href="https://www.prajwalkoirala.com/Important/vps_commands.php">VPS_Commands</a>
